e11622f6d868dce03ea33064d3d5b9a06ec15779
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-09-13 16:12:38 +0700

    Dispatch action

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+import createAction from '.';
+import { SET_TOKEN } from './type';
 export const signIn = (data) => (dispatch) => {
   axios({
     url: 'http://elearning0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap',
@@ -9,6 +10,8 @@ export const signIn = (data) => (dispatch) => {
     .then((res) => {
       console.log(res);
       localStorage.setItem('myToken', res.data.accessToken);
+
+      dispatch(createAction(SET_TOKEN, res.data.accessToken));
     })
     .catch((err) => {
       console.log(err);
--- src/redux/actions/type.js
+++ src/redux/actions/type.js
@@ -1,1 +1,2 @@
 export const SET_COURSES = 'course/SET_COURSE';
+export const SET_TOKEN = 'credentials/SET_TOKEN';
