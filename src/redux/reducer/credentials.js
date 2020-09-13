85bdc9e2095a6ef8ccbb1d43464a0e5ad8e2c564
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-09-13 16:16:18 +0700

    update switch case reducer

--- src/redux/reducer/credentials.js
+++ src/redux/reducer/credentials.js
@@ -1,9 +1,13 @@
+import { SET_TOKEN } from '../actions/type';
+
 let initialState = {
   token: '',
 };
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case SET_TOKEN:
+      return { ...state, token: payload };
     default:
       return state;
   }
